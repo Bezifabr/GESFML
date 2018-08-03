89115493c7fec786811321da76b42c24c0a4a5df
Bezifabr <maciej.saranda@outlook.com>
2018-08-03 19:59:37 +0200

    Added Entity identification system

--- include/GESFML/Entity.h
+++ include/GESFML/Entity.h
@@ -1,12 +1,25 @@
 #ifndef GESFML_ENTITY_H
 #define GESFML_ENTITY_H
 
+#include <string>
+#include <queue>
+
 namespace GESFML{
         
     class Entity
     {
+        unsigned long int id;
+        static unsigned long int idCount;
+        static std::queue<unsigned long int> removedIdQueue;
+
+
+        std::string name;
     public:
-        long long id;
+        Entity(std::string name);
+        ~Entity();
+
+        unsigned long int GetId() const;
+        std::string GetName() const;
     };
 }
 #endif // GESFML_ENTITY_H
--- src/GESFML/Entity.cpp
+++ src/GESFML/Entity.cpp
@@ -1,1 +1,33 @@
 #include "Entity.h"
+
+namespace GESFML{
+
+    unsigned long int Entity::idCount = 0;
+    std::queue<unsigned long int> Entity::removedIdQueue;
+
+    Entity::Entity(std::string name) : name(name)
+    {
+        if (!removedIdQueue.empty())
+        {
+            id = removedIdQueue.front();
+            removedIdQueue.pop();
+        }
+        else id = idCount++;
+    }
+
+    Entity::~Entity()
+    {
+        removedIdQueue.push(id);
+    }
+
+    unsigned long int Entity::GetId() const
+    {
+        return id;
+    }
+
+    std::string Entity::GetName() const
+    {
+        return name;
+    }
+
+}
