358ee77133516a535283284559e0fa41d5f83519
bezifabr <bezifabr@gmail.com>
2018-08-14 12:12:12 +0200

    Added conversion from string to char*

--- include/GESFML/Config.h
+++ include/GESFML/Config.h
@@ -4,6 +4,6 @@ namespace GESFML{
 constexpr float WINDOW_WIDTH = 1000;
 constexpr float WINDOW_HEIGHT = 1000;
 
-constexpr char* WINDOW_TITLE = "GESFML";
+constexpr char* WINDOW_TITLE = (char*)"GESFML";
 
 }
